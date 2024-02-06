43bf970a53cde53ad91cd44ad8be3bcd5bd7f8fd
Henrry Bran <2168036320108@ingenieria.usac.edu.gt>
2024-02-06 17:27:24 -0600

    add nodeback
    Se agrego la parte de node al proyecto

--- Tareas/Tarea-No2/Frontend/reactfront/src/App.js
+++ Tareas/Tarea-No2/Frontend/reactfront/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 import Webcam from 'react-webcam';
 import axios from 'axios';
+import './App.css';
 
 function App() {
   const webcamRef = useRef(null);
@@ -14,8 +15,12 @@ function App() {
   const sendImageToServer = async () => {
     try {
       const currentDate = new Date().toISOString();
+      // Eliminar la parte "data:image/webp;base64," de la cadena image
+      const commaIndex = image.indexOf(',');
+      const base64Data = image.substring(commaIndex + 1);
+
       const response = await axios.post('http://localhost:5000/upload', {
-        image: image,
+        image: base64Data, // Utilizar la cadena base64 sin la metadata
         date: currentDate,
       });
 
@@ -27,7 +32,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>React Node Camera App</h1>
+      <h1>Tarea No. 2 SO1 201314439</h1>
       <Webcam ref={webcamRef} />
       <br />
       <button onClick={captureImage}>Capture Image</button>
@@ -35,7 +40,9 @@ function App() {
         <div>
           <img src={image} alt="Captured" />
           <br />
-          <button onClick={sendImageToServer}>Send to Node.js</button>
+          <div style={{ textAlign: 'center' }}>
+            <button className="send-button" onClick={sendImageToServer}>Send to Node.js</button>
+          </div>
         </div>
       )}
     </div>
